8ef501c0bf61cf1d73e3671ce3d7ca4b22d53fcc
HappyMan17 <sebasgrisales16@gmail.com>
2023-10-01 21:46:19 -0500

    feat(Home): Background image implemented for Home page.

--- jobmatch/src/App.tsx
+++ jobmatch/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './components/home/home.tsx';
+import Home from './components/home/Home.tsx';
 import UserRegister from './components/register/userRegister.tsx';
 import WorkerRegister from './components/register/workerRegister.tsx';
 
