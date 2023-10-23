c7138016dd76fc4631d1dd2f107c10189bc9ec06
sxngg <santiagogarciagarzon.8@gmail.com>
2023-10-22 23:18:10 -0500

    feat(Home): Home screen refactorized for clean code, and navigate for login added

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,85 +6,14 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  Box,
   Link,
-  styled,
 } from '@mui/material';
 
 //Icons
 import LoginIcon from '@mui/icons-material/Login';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
-
-const Root = styled(Box)({
-  minHeight: '100vh',
-  minWidth: '100vw',
-  backgroundImage: 'url("src/assets/Principal.jpg")',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  display: 'flex',
-  flexDirection: 'column',
-});
-
-const Footer = styled('footer')({
-  backgroundColor: 'whitesmoke',
-  color: 'black',
-  padding: 2,
-  textAlign: 'center',
-  marginTop: 4,
-});
-
-const useStyles = {
-  appBar: {
-    backgroundColor: 'whitesmoke',
-  },
-  container: {
-    marginTop: 5,
-    marginBottom: 10,
-  },
-  card: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s ease-in-out, transform 0.6s ease-in-out',
-    '&:hover': {
-      backgroundColor: '#c6ccc9',
-      transform: 'scale(1.05)', 
-    },
-    padding: 5,
-    maxHeight: 150,
-  },
-  cardContent: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  logo_container: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    alignContent: 'center',
-    marginBottom: 5,
-  },
-  title: {
-    fontFamily: 'sans-serif',
-    fontSize: '32px',
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 5,
-    letterSpacing: 5,
-    textTransform: 'uppercase',
-  },
-  description: {
-    fontSize: '18px',
-    color: 'whitesmoke',
-    letterSpacing: 5,
-  },
-};
+import { Footer, Root } from '../materialUI-common';
+import { useStyles } from './HomeStyles';
 
 function Home() {
   return (
@@ -117,7 +46,7 @@ function Home() {
         </Container>
         <Grid container spacing={3} sx={{display: 'flex'}}>
           <Grid item xs={12} sm={6} >
-            <Link href="/register" underline='none'>
+            <Link href="/login" underline='none'>
               <Paper sx={useStyles.card}>
                 <LoginIcon sx={{fontSize: 140}} />
                 <Container sx={useStyles.cardContent}>
