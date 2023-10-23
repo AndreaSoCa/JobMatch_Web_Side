943e71c0fcaab72a8559456d05a8a87574043d8b
sxngg <santiagogarciagarzon.8@gmail.com>
2023-10-22 23:38:38 -0500

    fix(Login): problem fixed and form added with his states

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -61,7 +61,95 @@ const LoginForm = () => {
   return (
     <Root>
       <Grid container component="main" sx={{ height: "100vh"}}>
-        
+        <Grid item xs={12} sm={12} md={12}
+          sx={{display: 'flex', justifyContent: 'center',
+            alignItems: 'center' }}>
+          <Box
+            sx={useStyles.form_container}
+          >
+            <img
+              src="src/assets/tortuga.png"
+              alt="Image description"
+              style={{ width: '150px', height: '100px' }}
+            />
+            <Typography component="h1" variant="h5" sx={useStyles.title}>
+              Come in and connect now
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    margin="normal"
+                    type="text"
+                    fullWidth
+                    label="Email"
+                    sx={{ mt: 2, mb: 1.5 }}
+                    required
+                    {...register("email", { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i })}
+                  />
+                  {errors.email?.type === 'required' && <Typography sx={{color: 'black'}}>This field is required</Typography>}
+                  {errors.email?.type === 'pattern' && <Typography sx={{color: 'black'}}>This field must be email format</Typography>}
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    margin="normal"
+                    type="password"
+                    fullWidth
+                    label="Password"
+                    sx={{ mt: 1.5, mb: 1.5 }}
+                    required
+                    {...register("password", { required: true, minLength: 8 })}
+                  />
+                  {errors.password?.type === 'required' && <Typography sx={{color: 'black'}}>This field is required</Typography>}
+                  {errors.password?.type === 'minLength' && <Typography sx={{color: 'black'}}>This field must be minimun 8 chars</Typography>}
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <FormControl fullWidth>
+                    <InputLabel id="login-type-select">
+                      Login type
+                    </InputLabel>
+                    <Select
+                      labelId="login-type-select"
+                      id="login-type-select"
+                      label="Type of login"
+                      value={typeLogin}
+                      defaultValue="user"
+                      onChange={handleChangeTypeLogin}
+                    >
+                      <MenuItem value={"user"}>User</MenuItem>
+                      <MenuItem value={"worker"}>Worker</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                //disabled={!isValid}
+              >
+                Log in
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link to="/register">You don't have an account? create one now</Link>
+                </Grid>
+              </Grid>
+              {alert.type === "success" && (
+                <Alert severity="success">{alert.message}</Alert>
+              )}
+              {alert.type === "error" && (
+                <Alert severity="error">{alert.message}</Alert>
+              )}
+            </Box>
+          </Box>
+        </Grid>
       </Grid>
       
     </Root>
