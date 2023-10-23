7a54e6fcc5183d70f05a6c3e8d9e22ef56553c15
sxngg <santiagogarciagarzon.8@gmail.com>
2023-10-22 23:14:24 -0500

    feat: new path for worker and user login in constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -3,7 +3,9 @@
 const CONSTANTS = {
     BASE_URL: 'http://localhost:3000/job-match',
     WORKER_REGISTER: '/worker/add',
+    WORKER_LOGIN: '/worker/login',
     USER_REGISTER: '/user/add',
+    USER_LOGIN: '/user/login',
   };
   
   export default CONSTANTS;
